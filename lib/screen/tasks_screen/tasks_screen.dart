2f64ba63b4e8147d818cf95259d6a88920296d6a
E7erlasting971 <91707431+E7erlasting971@users.noreply.github.com>
2022-10-22 18:59:26 +0700

    failed login

--- lib/screen/tasks_screen/tasks_screen.dart
+++ lib/screen/tasks_screen/tasks_screen.dart
@@ -87,11 +87,12 @@ class _TasksScreenState extends BaseScreenState<TasksScreen, TasksScreenStore> {
                               return ((store.tasks
                                           .elementAt(index)
                                           .taskIsDone ==
-                                      1)
+                                      0)
                                   ? ItemTask(
                                       onPressed: () {},
                                       onPressedComplete: () {},
-                                      time: '10/05/2022',
+                                      time: store.convertTimeTask(
+                                          store.tasks.elementAt(index)),
                                       title: store.tasks
                                               .elementAt(index)
                                               .taskSummary ??
@@ -99,10 +100,10 @@ class _TasksScreenState extends BaseScreenState<TasksScreen, TasksScreenStore> {
                                       isCompleted: ((store.tasks
                                                       .elementAt(index)
                                                       .taskIsDone ==
-                                                  1)
+                                                  0)
                                               ? true
                                               : false) ==
-                                          true,
+                                          false,
                                     )
                                   : const SizedBox.shrink());
                             },
@@ -132,20 +133,21 @@ class _TasksScreenState extends BaseScreenState<TasksScreen, TasksScreenStore> {
                               return ((store.tasks
                                           .elementAt(index)
                                           .taskIsDone ==
-                                      0)
+                                      1)
                                   ? ItemTask(
                                       onPressed: () {},
                                       onPressedComplete: () {},
-                                      time: '10/05/2022',
+                                      time: store.convertTimeTask(
+                                          store.tasks.elementAt(index)),
                                       title: store.tasks[index].taskSummary
                                           .toString(),
                                       isCompleted: ((store.tasks
                                                       .elementAt(index)
                                                       .taskIsDone ==
-                                                  0)
+                                                  1)
                                               ? true
                                               : false) ==
-                                          false,
+                                          true,
                                     )
                                   : const SizedBox.shrink());
                             },
