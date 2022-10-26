8987bf480a9b7f61565623479022e2c85810057d
Nguyễn Đức Hòa <nguyenduchoa.data@gmail.com>
2022-10-26 21:04:48 +0700

    Update profile_screen_store.dart

--- lib/screen/profile_screen/store/profile_screen_store.dart
+++ lib/screen/profile_screen/store/profile_screen_store.dart
@@ -99,9 +99,6 @@ abstract class _ProfileScreenStore with Store, BaseStoreMixin {
     Map<String, dynamic> params = {
       'id_user': _loginScreenStore.currentAccount.accountId.toString()
     };
-    Map<String, dynamic> params = {
-      'id_user': _loginScreenStore.currentAccount.accountId.toString()
-    };
     await _baseAPI
         .fetchData(
       ManagerAddress.workspacesGetAllByIdUser,
